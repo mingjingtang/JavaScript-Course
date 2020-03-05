4c42729f0808ac6531c79972874c0efacf84e426
mingjing tang <tmjcsh@gmail.com>
2020-03-04 23:42:45 -0500

    ES6

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -59,3 +59,55 @@ Person.prototype.myFriends = function(friends) {
 let friends = ["Bob", "Jane", "Jack"];
 new Person("John").myFriends(friends);
 ///////////////////////////////////////////////////////////////////////
+//destructuring
+const [name, age2] = ["John", "28"];
+console.log(name);
+console.log(age2);
+
+const john = {
+  birthday: "2 14",
+  favoriateColor: "Blue"
+};
+
+const { birthday, favoriateColor } = john;
+console.log(birthday);
+console.log(favoriateColor);
+
+const { birthday: b, favoriateColor: f } = john;
+console.log(b);
+console.log(f);
+
+function calcRetirement(birthYear) {
+  let thisYear = new Date().getFullYear();
+  let age3 = thisYear - birthYear;
+  let retirementYear = 65 - age3 + thisYear;
+  return [age3, retirementYear];
+}
+
+const [age3, retirementYear] = calcRetirement(1989);
+console.log(age3, retirementYear);
+
+//////////////////////////////////////////////////////////////////////////
+//Array example 1
+const boxes = document.querySelectorAll(".box");
+
+Array.from(boxes).forEach(cur => (cur.style.backgroundColor = "dodgerblue"));
+
+const boxesArr = Array.from(boxes);
+
+for (const cur of boxesArr) {
+  //   if (cur.className === "box blue") {
+  //     continue;
+  //   }
+
+  if (cur.className.includes("blue")) {
+    continue;
+  }
+  cur.textContent = "I changed to blue!";
+}
+
+//Array example 2
+const ages = [12, 5, 21, 7, 10];
+
+console.log(ages.findIndex(cur => cur > 18));
+console.log(ages.find(cur => cur > 18));
